d24a803ed7078bf2f7ca719919514fb684c865c9
Edward Chiang <edwardubuntu@gmail.com>
2015-05-17 14:31:12 +0800

    Initail create

---
+++ app/src/main/java/tw/soleil/loginview/LoginView.java
@@ -0,0 +1,9 @@
+package tw.soleil.loginview;
+
+/**
+ * Created by edward_chiang on 15/5/17.
+ */
+public class LoginView {
+
+    public static final String TAG = "LoginView";
+}
---
+++ app/src/main/java/tw/soleil/loginview/activity/ViewPagerLoginActivity.java
@@ -0,0 +1,12 @@
+package tw.soleil.loginview.activity;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by edward_chiang on 15/5/17.
+ */
+public class ViewPagerLoginActivity extends Activity {
+
+    public static final int LOGINCODE = 101;
+}
